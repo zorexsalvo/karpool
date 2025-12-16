52a35aeccc4875f109b0146ccf7ff1c0943e1b5d
Zorex Salvo <zorexsalvo@gmail.com>
2025-12-16 17:19:48 +0800

    Add QR code URL route

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('event/<slug:slug>/', views.event_detail, name='event_detail'),
+    path('event/<slug:slug>/qr/', views.event_qr, name='event_qr'),
     path('event/<slug:slug>/add-car/', views.add_car, name='add_car'),
     path('event/<slug:slug>/add-member/', views.add_member, name='add_member'),
     path('event/<slug:slug>/member/<int:member_id>/update/', views.update_member, name='update_member'),
